c527f910394c6f3c3e9f18799ffe297c8477dc35
Zak Nesler <zaknesler@gmail.com>
2018-08-10 14:25:32 -0400

    wip

--- resources/views/settings/partials/_sidebar.blade.php
+++ resources/views/settings/partials/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <ul class="h-full list-reset">
     <li><a class="block hover:bg-grey-lightest text-grey-darker no-underline px-6 py-4 {{ Route::currentRouteIs('settings.account', 'bg-grey-lightest hover:bg-grey-lightest font-semibold text-blue border-r-2 -mr-px border-blue') }}" href="{{ route('settings.account') }}">Your Account</a></li>
-    <li><a class="block hover:bg-grey-lightest text-grey-darker no-underline px-6 py-4 {{ Route::currentRouteIs('settings.social', 'bg-grey-lightest hover:bg-grey-lightest font-semibold text-blue border-r-2 -mr-px border-blue') }}" href="{{ route('settings.social') }}">Social Login</a></li>
+    <li><a class="block hover:bg-grey-lightest text-grey-darker no-underline px-6 py-4 {{ Route::currentRouteIs('settings.social', 'bg-grey-lightest hover:bg-grey-lightest font-semibold text-blue border-r-2 -mr-px border-blue') }}" href="{{ route('settings.social') }}">Authentication</a></li>
 
     <li><a class="block hover:bg-grey-lightest text-grey-darker no-underline px-6 py-4" href="#" @click.prevent="logout">Logout</a></li>
 </ul>
